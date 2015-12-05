6f06d0032bb03fd629e1a66c28fc6a2a33938f27
carlosssenai <carlos_ribeiro.15@hotmail.com>
2015-12-05 15:09:59 -0200

    SalaoBelezaVip

---
+++ Salao.BelezaVip.Test/BelezaVipUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Salao.BelezaVip.Test
+{
+    [TestClass]
+    public class BelezaVipUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
